b811cc8b7e81ecfec925c43492e6d14fe1c26b61
Sean <scolasito94@gmail.com>
2022-10-12 16:26:37 -0400

    Added cancellation, confirm, and FAQ pages.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,36 @@ function App() {
             <Route path="/" exact
                    element={<HomePage />}
             />
+            {/*Cancellation*/}
+            <Route path="/CancelAppointment" exact
+                   element={<CancelAppointment />}
+            />
+            <Route path="/ConfirmCancellation" exact
+                   element={<ConfirmCancellation />}
+            />
+            <Route path="/CancellationConfirmed" exact
+                   element={<CancellationConfirmed />}
+            />
+            {/*Confirmation*/}
+            <Route path="/ConfirmAppointment" exact
+                   element={<ConfirmAppointment />}
+            />
+            <Route path="/EnterInformation" exact
+                   element={<EnterInformation />}
+            />
+            <Route path="/ConfirmInformation" exact
+                   element={<ConfirmInformation />}
+            />
+            <Route path="/AppointmentConfirmed" exact
+                   element={<AppointmentConfirmed />}
+            />
+            {/*FAQ*/}
+            <Route path="/FAQ" exact
+                   element={<FAQ />}
+            />
+            <Route path="/ContactUs" exact
+                   element={<ContactUs />}
+            />
           </Routes>
         </BrowserRouter>
       </header>
