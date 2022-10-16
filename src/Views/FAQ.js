67f3b9a3551e5fb135641777fcca0ef1e4978804
Sean <scolasito94@gmail.com>
2022-10-16 09:33:17 -0400

    Added content to FAQ page

--- src/Views/FAQ.js
+++ src/Views/FAQ.js
@@ -1,7 +1,23 @@
+import {Link} from "react-router-dom";
+
 function FAQ() {
     return (
         <>
             <h1>FAQ</h1>
+            <Link class="link" to="/Question1"> Question 1 </Link>
+            <Link class="link" to="/Question1"> Question 2 </Link>
+            <Link class="link" to="/Question1"> Question 3 </Link>
+            <br/>
+            <br/>
+            Question 1 Answer
+            <br/>
+            Question 2 Answer
+            <br/>
+            Question 3 Answer
+            <br/>
+            <br/>
+            Can't find the answer to your question? Call or email us by clicking the link below:
+            <Link class="link" to="/ContactUs"> Contact Us </Link>
         </>
     );
 }
